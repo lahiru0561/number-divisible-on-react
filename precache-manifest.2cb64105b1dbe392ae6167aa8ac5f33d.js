3898cfa9260a844f138b7baaf335d26e4bf37e8e
Lahiru Rananasinghe <lahiru0561@gmail.com>
2019-03-13 00:18:54 +1100

    Updates

---
+++ precache-manifest.2cb64105b1dbe392ae6167aa8ac5f33d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7939515d75baf6a4efe4",
+    "url": "/static/js/main.b7786215.chunk.js"
+  },
+  {
+    "revision": "ffde1f390e9b869fedaf",
+    "url": "/static/js/2.d7846e8f.chunk.js"
+  },
+  {
+    "revision": "7939515d75baf6a4efe4",
+    "url": "/static/css/main.09d73cb9.chunk.css"
+  },
+  {
+    "revision": "8cbbd243bce0370b84266c1ec2decaa4",
+    "url": "/index.html"
+  }
+];
