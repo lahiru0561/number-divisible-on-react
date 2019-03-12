792ba47ae892ccbfdc5c8ab9cf2320860df89e67
Lahiru Rananasinghe <lahiru0561@gmail.com>
2019-03-13 00:29:12 +1100

    removed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import Divisible from './components/Divisible';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="container">
-          <h1 style={{ width: 300, display: 'block', margin: 'auto' }}>
-            Divisible Test
-          </h1>
-          <Route exact path="/" component={Divisible} />
-        </div>
-      </Router>
+      <div className="container">
+        <h1 style={{ width: 300, display: 'block', margin: 'auto' }}>
+          Divisible Test
+        </h1>
+        <Divisible />
+      </div>
     );
   }
 }
